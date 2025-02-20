dab69b0c59b749b716bc8eab008dbcb2b58dc341
Yutaka Matsubara <matsubara@abby.co.jp>
2025-02-20 09:51:25 +0900

    feat: add octokit wrapper

---
+++ src/octokit.ts
@@ -0,0 +1,47 @@
+import { getInput, warning } from "@actions/core";
+import { Octokit } from "@octokit/action";
+import { retry } from "@octokit/plugin-retry";
+import { throttling } from "@octokit/plugin-throttling";
+
+const token = getInput("token") || process.env.GITHUB_TOKEN;
+
+const RetryAndThrottlingOctokit = Octokit.plugin(throttling, retry);
+
+export const octokit = new RetryAndThrottlingOctokit({
+  auth: `token ${token}`,
+  throttle: {
+    onRateLimit: (
+      retryAfter: number,
+      // biome-ignore lint/suspicious/noExplicitAny: <explanation>
+      options: any,
+      // biome-ignore lint/suspicious/noExplicitAny: <explanation>
+      _o: any,
+      retryCount: number,
+    ) => {
+      warning(
+        `Request quota exhausted for request ${options.method} ${options.url}
+  Retry after: ${retryAfter} seconds
+  Retry count: ${retryCount}
+  `,
+      );
+      if (retryCount <= 3) {
+        warning(`Retrying after ${retryAfter} seconds!`);
+        return true;
+      }
+    },
+    // biome-ignore lint/suspicious/noExplicitAny: <explanation>
+    onSecondaryRateLimit: (retryAfter: number, options: any) => {
+      warning(
+        `SecondaryRateLimit detected for request ${options.method} ${options.url} ; retry after ${retryAfter} seconds`,
+      );
+      // if we are doing a POST method on /repos/{owner}/{repo}/pulls/{pull_number}/reviews then we shouldn't retry
+      if (
+        options.method === "POST" &&
+        options.url.match(/\/repos\/.*\/.*\/pulls\/.*\/reviews/)
+      ) {
+        return false;
+      }
+      return true;
+    },
+  },
+});
